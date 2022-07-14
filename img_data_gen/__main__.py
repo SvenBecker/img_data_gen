b016b9b402f3320f82ce9ab7ae72ee5649b36eb9
Sven Becker <Sven.Becker@mgm-tp.com>
2022-07-14 08:59:44 +0200

    fixed minor stuff

--- img_data_gen/__main__.py
+++ img_data_gen/__main__.py
@@ -4,6 +4,6 @@ from img_data_gen.loaders import ImageLoader
 
 start = time.time()
 img_container = ImageLoader().load_all()
-ImageGenerator(input_image_mapping=img_container.input_image_map, background_image_array=img_container.bg_image_list).run(samples=20)
+ImageGenerator(input_image_mapping=img_container.input_image_map, bg_image_list=img_container.bg_image_list).run(samples=5)
 print(f'Run took {time.time() - start} seconds.')
 
