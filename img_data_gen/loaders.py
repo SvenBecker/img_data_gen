b62a9895dc5c0d9b3f190b75ac5dc828afc5e636
Marius Braakmann <marius.braakmann@protonmail.com>
2022-07-17 15:23:41 +0200

    add BoundingBox class

--- img_data_gen/loaders.py
+++ img_data_gen/loaders.py
@@ -6,6 +6,7 @@ from typing import Tuple, List, Dict
 
 from PIL import Image
 
+import numpy as np
 
 @dataclass
 class ImageContainer:
@@ -48,3 +49,52 @@ class ImageLoader:
 
     def _load_bg_image(self, filepath: Path) -> 'Image':
         return Image.open(filepath).convert('RGBA')
+
+@dataclass
+class BoundingBox:
+    """Represents a card bounding box. Takes a non-rotated input image as parameter"""
+
+    vertices: List[Tuple[int, int]]
+    center: Tuple[int, int]
+
+    def __init__(
+        self,
+        img: 'Image',
+        anchor: Tuple[int, int] = (0,0) 
+        ):
+        self.vertices = [(0,0)] * 4
+
+        #vertices defined in counter-clockwise direction starting with top left corner
+        self.vertices[0] = anchor
+        self.vertices[1] = (anchor[0], anchor[1]-img.size[1])
+        self.vertices[2] = (anchor[0] + img.size[0], anchor[1] - img.size[1])
+        self.vertices[3] = (anchor[0] + img.size[0], anchor[1])
+
+        self.center = (anchor[0] + img.size[0]/2, anchor[1] - img.size[1]/2)
+
+    def move(self, x: int = 0, y: int = 0) -> None:
+        "Moves boundingbox by specified amount. Positive directions: right/up"
+       
+        for i, vertex in enumerate(self.vertices):
+            self.vertices[i] = (vertex[0] + x, vertex[1] + y)
+        
+        self.center = (self.center[0] + x, self.center[1] + y)
+
+    def rotate(self, angle: int) -> None:
+        """Rotates boundingbox by specified angle in degrees, counterclockwise around its center"""
+
+        # rotational matrix
+        c, s = np.cos(np.radians(angle)), np.sin(np.radians(angle))
+        R = np.array([[c, -s], [s, c]])
+
+        # transform coordinates to move rotation center to origin
+        vertices_temp = np.array(self.vertices) - np.array(self.center)
+
+        # perform rotation
+        vertices_rotated = np.empty(shape=(4,2))
+        for i in range(4):
+            vertices_rotated[i] = R.dot(vertices_temp[i,:])
+        
+        # reverse transform and reformat
+        vertices_rotated = (vertices_rotated + np.array(self.center)).round(decimals=0).astype(int) # rounding causes inaccuracies after multiple sequential rotations
+        self.vertices = [tuple(line) for line in vertices_rotated]
