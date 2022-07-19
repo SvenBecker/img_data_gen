44ab9404c3e82656adee5e478ca04a892ef18fae
Sven Becker <Sven.Becker@mgm-tp.com>
2022-07-19 12:00:47 +0200

    code review changes

--- img_data_gen/loaders.py
+++ img_data_gen/loaders.py
@@ -4,9 +4,9 @@ from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 from typing import Tuple, List, Dict
 
+import numpy as np
 from PIL import Image
 
-import numpy as np
 
 @dataclass
 class ImageContainer:
@@ -50,51 +50,59 @@ class ImageLoader:
     def _load_bg_image(self, filepath: Path) -> 'Image':
         return Image.open(filepath).convert('RGBA')
 
+
 @dataclass
 class BoundingBox:
     """Represents a card bounding box. Takes a non-rotated input image as parameter"""
-
     vertices: List[Tuple[int, int]]
     center: Tuple[int, int]
 
-    def __init__(
-        self,
-        img: 'Image',
-        anchor: Tuple[int, int] = (0,0) 
-        ):
-        self.vertices = [(0,0)] * 4
+    @classmethod
+    def from_img(cls, img: 'Image', anchor: Tuple[int, int] = (0, 0)) -> 'BoundingBox':
+        """Returns a bounding box object based on provided image."""
+        # todo: isn't it `... + img_size[1]` because otherwise I'll receive negative values?
+        vertices: List[Tuple[int, int]] = [
+            anchor,
+            (anchor[0], anchor[1] - img.size[1]),
+            (anchor[0] + img.size[0], anchor[1] - img.size[1]),
+            (anchor[0] + img.size[0], anchor[1])
+        ]
+        center = (anchor[0] + img.size[0] / 2, anchor[1] - img.size[1] / 2)
+        return cls(vertices=vertices, center=center)
 
-        #vertices defined in counter-clockwise direction starting with top left corner
-        self.vertices[0] = anchor
-        self.vertices[1] = (anchor[0], anchor[1]-img.size[1])
-        self.vertices[2] = (anchor[0] + img.size[0], anchor[1] - img.size[1])
-        self.vertices[3] = (anchor[0] + img.size[0], anchor[1])
+    def move(self, x: int = 0, y: int = 0) -> 'BoundingBox':
+        """
+        Moves bounding box by specified amount. Positive directions: right/up and returns a new BoundingBox object.
+        """
+        vertices = [
+            (vertex[0] + x, vertex[1] + y)
+            for i, vertex in enumerate(self.vertices)
+        ]
+        center = (self.center[0] + x, self.center[1] + y)
+        return BoundingBox(vertices=vertices, center=center)
 
-        self.center = (anchor[0] + img.size[0]/2, anchor[1] - img.size[1]/2)
-
-    def move(self, x: int = 0, y: int = 0) -> None:
-        "Moves boundingbox by specified amount. Positive directions: right/up"
-       
-        for i, vertex in enumerate(self.vertices):
-            self.vertices[i] = (vertex[0] + x, vertex[1] + y)
-        
-        self.center = (self.center[0] + x, self.center[1] + y)
-
-    def rotate(self, angle: int) -> None:
-        """Rotates boundingbox by specified angle in degrees, counterclockwise around its center"""
+    def rotate(self, angle: int) -> 'BoundingBox':
+        """
+        Rotates bounding box by specified angle in degrees, counterclockwise around its center and returns a new
+        BoundingBox object.
+        """
 
         # rotational matrix
         c, s = np.cos(np.radians(angle)), np.sin(np.radians(angle))
-        R = np.array([[c, -s], [s, c]])
+        matrix = np.array([[c, -s], [s, c]])
 
         # transform coordinates to move rotation center to origin
         vertices_temp = np.array(self.vertices) - np.array(self.center)
 
         # perform rotation
-        vertices_rotated = np.empty(shape=(4,2))
-        for i in range(4):
-            vertices_rotated[i] = R.dot(vertices_temp[i,:])
+        vertices_rotated = [
+            matrix.dot(vertices_temp[i, :])
+            for i in range(4)
+        ]
         
         # reverse transform and reformat
-        vertices_rotated = (vertices_rotated + np.array(self.center)).round(decimals=0).astype(int) # rounding causes inaccuracies after multiple sequential rotations
-        self.vertices = [tuple(line) for line in vertices_rotated]
+        # rounding causes inaccuracies after multiple sequential rotations
+        vertices_rotated = (vertices_rotated + np.array(self.center)).round(decimals=0).astype(int)
+        vertices = [tuple(line) for line in vertices_rotated]
+
+        return BoundingBox(vertices=vertices, center=self.center)
